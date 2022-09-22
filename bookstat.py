292e9ac21f4748bb025ae4505de5f78c529d054e
rubenforti <rubenforti27@gmail.com>
2022-09-22 12:29:49 +0200

    Initial import

---
+++ bookstat.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# Copyright (C) 2022 luca.baldini@pi.infn.it
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+"""First assignment for the CMEPDA course, 2022/23.
+"""
+
+import argparse
+
+
+def process(file_path):
+    """
+    """
+    print(f'Opening input file {file_path}...')
+    with open(file_path, 'r') as input_file:
+        text = input_file.read()
+    print(text)
+    print('Done.')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Print some book statistics')
+    parser.add_argument('infile', type=str, help='path to the input file')
+    args = parser.parse_args()
+    process(args.infile)

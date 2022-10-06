134e22c307b9ad05863ea8fb8576f24e4a8a9560
rubenforti <rubenforti27@gmail.com>
2022-10-06 12:09:55 +0200

    Minor details

--- bookstat.py
+++ bookstat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# Copyright (C) 2022 luca.baldini@pi.infn.it
+# Copyright (C) 2022 r.forti1@studenti.unipi.it
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,8 +14,23 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""First assignment for the CMEPDA course, 2022/23.
-"""
+"""First assignment for the CMEPDA course, 2022/23. """
+
+# --- Goal
+# Write a Python program that prints the relative frequence of each letter
+# of the alphabet (without distinguishing between lower and upper case) in the
+# book.
+
+# --- Specifications
+# - the program should have a --help option summarizing the usage
+# - the program should accept the path to the input file from the command line
+# - the program should print out the total elapsed time
+# - the program should have an option to display a histogram of the frequences
+# - [optional] the program should have an option to skip the parts of the text
+#   that do not pertain to the book (e.g., preamble and license)
+# - [optional] the program should have an option to print out the basic book
+#   stats (e.g., number of characters, number of words, number of lines, etc.)
+
 
 import time
 import argparse
